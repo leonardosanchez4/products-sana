f9d31281e274764495b2a9c578edc83f2ad385a6
leonardo <leonardo@LEONARDO-PC>
2017-08-24 20:12:35 -0500

    a

--- Product-Sana/WebSite/Global.asax.cs
+++ Product-Sana/WebSite/Global.asax.cs
@@ -16,6 +16,7 @@ namespace WebSite
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
         }
     }
 }
